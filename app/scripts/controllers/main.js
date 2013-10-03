5d9021ae6942819e738be0d488ee37be8a3bf77b
Saurabh <thes.kumar@gmail.com>
2013-10-03 17:41:22 +0530

    Initial Commit!

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('ordioApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
